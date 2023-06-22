83811023041d47b9b5c2a94ecc3fd7c6ee382c56
amna-khan <amna.khan@ukaea.uk>
2023-06-22 19:30:24 +0100

    changed formatting of added user objects block

--- moose/userobjects.py
+++ moose/userobjects.py
@@ -1,15 +1,20 @@
-#!/usr/env/python3
+class UserObjects:
+    def __init__(self, name="UserObjects"):
+        self.name = name
+        self.sections = []
 
-from enum import IntEnum, auto
-
-class UserObjects():
-    def __init__(self, **kwargs):
-        self.name = "UserObjects"
-        self.solve_objects = kwargs
+    def add_section(self, section_name, **kwargs):
+        section = {section_name: kwargs}
+        self.sections.append(section)
 
     def __str__(self):
-        string =  f'[{self.name}]\n'
-        for key in self.solve_objects.keys():
-            string += f'{key}="{self.solve_objects[key]}"\n'
-        string += '[]\n'
+        string = f"[{self.name}]\n"
+        for section in self.sections:
+            section_name = list(section.keys())[0]
+            string += f"  [{section_name}]\n"
+            attributes = section[section_name]
+            for key in attributes.keys():
+                string += f"    {key} = {attributes[key]}\n"
+            string += "  []\n"
+        string += "[]\n"
         return string
